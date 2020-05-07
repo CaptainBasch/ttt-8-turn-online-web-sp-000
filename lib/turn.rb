d95f457ba2197a53fc44f958228314b67458a0c8
Bailey Davis <bailey.gage.davis@gmail.com>
2020-05-07 17:02:35 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -2,7 +2,7 @@ def turn(board)
   puts "Please enter 1-9:"
   input = gets.chomp
   input = input_to_index(input)
-  until valid_move?(board, input)
+  while valid_move?(board, input)
     turn(board)
   end
   move(board, input)
